a5b8063c6fa6ca29afa2c303bdc0e724ecc20a29
Raghav Sharma <raghavs@adobe.com>
2025-12-01 13:35:07 +0530

    updated the readme; added a way to create hermetic shells

---
+++ default.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+      (pkgs.python312.withPackages (python-pkgs: with python-pkgs; [
+        # select Python packages here
+        requests
+        python-dotenv
+      ]))
+    ];
+}
